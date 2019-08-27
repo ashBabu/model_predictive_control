9742c3d394ecf989f273a1153d5be30fccddd7ad
Ash <shyamashi@gmail.com>
2019-08-27 15:38:18 +0100

    animation part updated

--- src/dh_plotter.py
+++ src/dh_plotter.py
@@ -1,3 +1,119 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
+#
+#
+# def update_line(num, data, line):
+#     line.set_data(data[..., :num])
+#     return line,
+#
+# fig1 = plt.figure()
+#
+# # Fixing random state for reproducibility
+# np.random.seed(19680801)
+#
+# data = np.random.rand(2, 25)
+# l, = plt.plot([], [], 'r-')
+# plt.xlim(0, 1)
+# plt.ylim(0, 1)
+# plt.xlabel('x')
+# plt.title('test')
+# line_ani = animation.FuncAnimation(fig1, update_line, 25, fargs=(data, l),
+#                                    interval=50, blit=True)
+# plt.show()
+#
+# # To save the animation, use the command: line_ani.save('lines.mp4')
+#
+# from numpy import sin, cos
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import scipy.integrate as integrate
+# import matplotlib.animation as animation
+#
+# G = 9.8  # acceleration due to gravity, in m/s^2
+# L1 = 1.0  # length of pendulum 1 in m
+# L2 = 1.0  # length of pendulum 2 in m
+# M1 = 1.0  # mass of pendulum 1 in kg
+# M2 = 1.0  # mass of pendulum 2 in kg
+#
+#
+# def derivs(state, t):
+#
+#     dydx = np.zeros_like(state)
+#     dydx[0] = state[1]
+#
+#     delta = state[2] - state[0]
+#     den1 = (M1+M2) * L1 - M2 * L1 * cos(delta) * cos(delta)
+#     dydx[1] = ((M2 * L1 * state[1] * state[1] * sin(delta) * cos(delta)
+#                 + M2 * G * sin(state[2]) * cos(delta)
+#                 + M2 * L2 * state[3] * state[3] * sin(delta)
+#                 - (M1+M2) * G * sin(state[0]))
+#                / den1)
+#
+#     dydx[2] = state[3]
+#
+#     den2 = (L2/L1) * den1
+#     dydx[3] = ((- M2 * L2 * state[3] * state[3] * sin(delta) * cos(delta)
+#                 + (M1+M2) * G * sin(state[0]) * cos(delta)
+#                 - (M1+M2) * L1 * state[1] * state[1] * sin(delta)
+#                 - (M1+M2) * G * sin(state[2]))
+#                / den2)
+#
+#     return dydx
+#
+# # create a time array from 0..100 sampled at 0.05 second steps
+# dt = 0.05
+# t = np.arange(0, 20, dt)
+#
+# # th1 and th2 are the initial angles (degrees)
+# # w10 and w20 are the initial angular velocities (degrees per second)
+# th1 = 120.0
+# w1 = 0.0
+# th2 = -10.0
+# w2 = 0.0
+#
+# # initial state
+# state = np.radians([th1, w1, th2, w2])
+#
+# # integrate your ODE using scipy.integrate.
+# y = integrate.odeint(derivs, state, t)
+#
+# x1 = L1*sin(y[:, 0])
+# y1 = -L1*cos(y[:, 0])
+#
+# x2 = L2*sin(y[:, 2]) + x1
+# y2 = -L2*cos(y[:, 2]) + y1
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
+# ax.set_aspect('equal')
+# ax.grid()
+#
+# line, = ax.plot([], [], 'o-', lw=2)
+# time_template = 'time = %.1fs'
+# time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+#
+#
+# def init():
+#     line.set_data([], [])
+#     time_text.set_text('')
+#     return line, time_text
+#
+#
+# def animate(i):
+#     thisx = [0, x1[i], x2[i]]
+#     thisy = [0, y1[i], y2[i]]
+#
+#     line.set_data(thisx, thisy)
+#     time_text.set_text(time_template % (i*dt))
+#     return line, time_text
+#
+#
+# ani = animation.FuncAnimation(fig, animate,
+#                               interval=dt*1000, blit=True, init_func=init)
+# plt.show()
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 # import tf.transformations as tf_tran
@@ -37,17 +153,22 @@ class DH_plotter():
         T_joint[i+1, :, :] = T_ee
         return T_joint
 
-    def plotter(self, ax, T_joint, lgnd, color='r'):
-        x, y, z = 0, 0, 0
-        for i in range(T_joint.shape[0]):
-            ax.plot([x, T_joint[i, 0, 3]], [y, T_joint[i, 1, 3]], [z, T_joint[i, 2, 3]], color, label=lgnd)
-            ax.scatter(T_joint[i, 0, 3], T_joint[i, 1, 3], T_joint[i, 2, 3], 'gray')
-            x, y, z = T_joint[i, 0, 3], T_joint[i, 1, 3], T_joint[i, 2, 3]
-        # ax.arrow(0.0, 0.0, 0.0, 0., 0.15, 0., head_width=0.05, head_length=0.1, fc='k', ec='k')  # inertial y axis
-        plt.xlabel('X')
-        plt.ylabel('Y')
-        ax.axis('equal')
-        ax.set_zlim(0, 1)
+    def plotter(self, ax, Tt, lgnd, color='r'):
+        for j in range(len(Tt)):
+            x, y, z = 0, 0, 0
+            T_joint = Tt[j]
+            for i in range(T_joint.shape[0]):
+                ax.plot([x, T_joint[i, 0, 3]], [y, T_joint[i, 1, 3]], [z, T_joint[i, 2, 3]], color, lw=10, label=lgnd)
+                ax.scatter(T_joint[i, 0, 3], T_joint[i, 1, 3], T_joint[i, 2, 3], 'gray', lw=10)
+                x, y, z = T_joint[i, 0, 3], T_joint[i, 1, 3], T_joint[i, 2, 3]
+                # ax.axis('equal')
+                ax.set_zlim(0., 1.)
+                ax.set_ylim(0., 2.)
+                ax.set_xlim(-1., 1.)
+                plt.xlabel('X')
+                plt.ylabel('Y')
+            plt.pause(0.01)
+            plt.cla()
 
 
 if __name__ == '__main__':
@@ -61,9 +182,17 @@ if __name__ == '__main__':
     robot = '3DOF'
     nDoF = 3
     # the manipulator is a straight line when the joint angles are [0, 90, , 0]
-    q = np.array([np.pi/4*0, np.pi/2, np.pi/2*0])
+    q = np.array([np.pi/4*0, np.pi/2, np.pi/2])
     dh_plotter = DH_plotter(nDoF, robot)
-    T_joint = dh_plotter.robot_DH_matrix(q)
-    dh_plotter.plotter(ax, T_joint, 'desired', color='blue')
+    q3 = np.linspace(0, np.pi/2, 50)
+    q1 = np.linspace(0, np.pi, 50)
+    Tt = list()
+    for i in range(len(q3)):
+        # q = np.array([0, np.pi/2, q3[i]])
+        q = np.array([q1[i], np.pi/2, np.pi/2])
+        # q = np.array([0, np.pi/2, q3[i]])
+        T_joint = dh_plotter.robot_DH_matrix(q)
+        Tt.append(T_joint)
+    dh_plotter.plotter(ax, Tt, 'desired', color='blue')
 
     plt.show()
