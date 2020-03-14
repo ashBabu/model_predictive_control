e0898218ae056e42fdec1b6722e0217bcb76f8b6
Ash Babu <shyamashi@gmail.com>
2020-03-14 11:06:21 +0000

    restored as Geri's promp

--- src/iros/promps.py
+++ src/iros/promps.py
@@ -23,12 +23,9 @@ class ProMP:
         weights = np.random.multivariate_normal(self.mu, self.covMat, n_samples)
         weights = weights.transpose()
         trajectoryFlat = basisMultiDoF.dot(weights)
-        #a = trajectoryFlat
-        trajectoryFlat = trajectoryFlat.reshape((self.numDoF,int(trajectoryFlat.shape[0] / self.numDoF), n_samples))
-        trajectoryFlat = np.transpose(trajectoryFlat)
-        # trajectoryFlat = np.transpose(trajectoryFlat, (1, 0, 2))
-        #trajectoryFlat = trajectoryFlat.reshape((a.shape[0] / self.numDoF, self.numDoF, n_samples))
-
+        trajectoryFlat = trajectoryFlat.reshape((self.numDoF, int(trajectoryFlat.shape[0] / self.numDoF), n_samples))
+        # trajectoryFlat = np.transpose(trajectoryFlat)
+        trajectoryFlat = np.transpose(trajectoryFlat, (1, 0, 2))
         return trajectoryFlat
 
     def getMeanAndCovarianceTrajectory(self, time):
@@ -37,36 +34,28 @@ class ProMP:
         trajectoryMean = trajectoryFlat.reshape((self.numDoF, int(trajectoryFlat.shape[0] / self.numDoF)))
         trajectoryMean = np.transpose(trajectoryMean, (1, 0))
         covarianceTrajectory = np.zeros((self.numDoF, self.numDoF, len(time)))
-
         for i in range(len(time)):
-
             basisSingleT = basisMultiDoF[slice(i, (self.numDoF - 1) * len(time) + i + 1, len(time)), :]
             covarianceTimeStep = basisSingleT.dot(self.covMat).dot(basisSingleT.transpose())
             covarianceTrajectory[:, :, i] = covarianceTimeStep
-
         return trajectoryMean, covarianceTrajectory
 
     def getMeanAndStdTrajectory(self, time):
         basisMultiDoF = self.basis.basisMultiDoF(time, self.numDoF)
         trajectoryFlat = basisMultiDoF.dot(self.mu.transpose())
-        trajectoryMean = trajectoryFlat.reshape((self.numDoF, trajectoryFlat.shape[0] / self.numDoF))
+        trajectoryMean = trajectoryFlat.reshape((self.numDoF, int(trajectoryFlat.shape[0] / self.numDoF)))
         trajectoryMean = np.transpose(trajectoryMean, (1, 0))
         stdTrajectory = np.zeros((len(time), self.numDoF))
-
         for i in range(len(time)):
-
             basisSingleT = basisMultiDoF[slice(i, (self.numDoF - 1) * len(time) + i + 1, len(time)), :]
             covarianceTimeStep = basisSingleT.dot(self.covMat).dot(basisSingleT.transpose())
             stdTrajectory[i, :] = np.sqrt(np.diag(covarianceTimeStep))
-
         return trajectoryMean, stdTrajectory
 
     def getMeanAndCovarianceTrajectoryFull(self, time):
         basisMultiDoF = self.basis.basisMultiDoF(time, self.numDoF)
-
         meanFlat = basisMultiDoF.dot(self.mu.transpose())
         covarianceTrajectory = basisMultiDoF.dot(self.covMat).dot(basisMultiDoF.transpose())
-
         return meanFlat, covarianceTrajectory
 
     def jointSpaceConditioning(self, time, desiredTheta, desiredVar):
@@ -79,29 +68,21 @@ class ProMP:
         newProMP.covMat = self.covMat - L.dot(basisMatrix).dot(self.covMat)
         return newProMP
 
-
     def getTrajectoryLogLikelihood(self, time , trajectory):
-
         trajectoryFlat =  trajectory.transpose().reshape(trajectory.shape[0] * trajectory.shape[1])
         meanFlat, covarianceTrajectory = self.getMeanAndCovarianceTrajectoryFull(self, time)
-
         return stats.multivariate_normal.logpdf(trajectoryFlat, mean=meanFlat, cov=covarianceTrajectory)
 
     def getWeightsLogLikelihood(self, weights):
-
         return stats.multivariate_normal.logpdf(weights, mean=self.mu, cov=self.covMat)
 
     def plotProMP(self, time, indices = None):
         import plotter as plotter
-
         trajectoryMean, stdTrajectory = self.getMeanAndStdTrajectory(time)
-
-
         plotter.plotMeanAndStd(time, trajectoryMean, stdTrajectory, indices)
 
 
-
-class MAPWeightLearner():
+class MAPWeightLearner:
 
     def __init__(self, proMP, regularizationCoeff=10**-9, priorCovariance=10**-4, priorWeight=1):
         self.proMP = proMP
@@ -110,11 +91,9 @@ class MAPWeightLearner():
         self.regularizationCoeff = regularizationCoeff
 
     def learnFromData(self, trajectoryList, timeList):
-
         numTraj = len(trajectoryList)
         weightMatrix = np.zeros((numTraj, self.proMP.numWeights))
         for i in range(numTraj):
-
             trajectory = trajectoryList[i]
             time = timeList[i]
             trajectoryFlat = trajectory.transpose().reshape(trajectory.shape[0] * trajectory.shape[1])
@@ -122,9 +101,7 @@ class MAPWeightLearner():
             temp = basisMatrix.transpose().dot(basisMatrix) + np.eye(self.proMP.numWeights) * self.regularizationCoeff
             weightVector = np.linalg.solve(temp, basisMatrix.transpose().dot(trajectoryFlat))
             weightMatrix[i, :] = weightVector
-
         self.proMP.mu = np.mean(weightMatrix, axis=0)
-
         sampleCov = np.cov(weightMatrix.transpose())
         self.proMP.covMat = (numTraj * sampleCov + self.priorCovariance * np.eye(self.proMP.numWeights)) / (numTraj + self.priorCovariance)
 
@@ -140,8 +117,7 @@ if __name__ == "__main__":
     meanTraj, covTraj = proMP.getMeanAndCovarianceTrajectory(time)
     plotDof = 2
     # plt.figure()
-    # plt.plot(time, trajectories1[:, plotDof, :])
-    # #
+    # plt.plot(time, trajectories[:, plotDof, :])
     # plt.figure()
     # plt.plot(time, meanTraj[:, 0])
 
@@ -180,7 +156,7 @@ if __name__ == "__main__":
     plt.plot(time, trajectories[:, plotDof, :])
     plt.xlabel('time')
     plt.title('Joint-Space conditioning')
-    newProMP.plotProMP(time, [3,4])
+    newProMP.plotProMP(time, [3, 4])
 
     plt.show()
 
